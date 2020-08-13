e0fc2462ecb3c6c9c36eb16ed9617fdabfc2647d
freitassp <priettosamuel@gmail.com>
2020-08-13 03:55:22 -0300

    Refencia Treinando
    Tirando duvidas sobre referencia e treinando para evitar o null

---
+++ EnderecoPessoa/src/enderecopessoa/Pessoa.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enderecopessoa;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Pessoa {
+    
+    String nome;
+    String cpf;
+    int idade;
+    Endereco endereco;
+    
+    
+}
