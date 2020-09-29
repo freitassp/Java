e3313da5f57c2a26a027e8096cbfb855d7a86b6f
freitassp <priettosamuel@gmail.com>
2020-09-29 03:38:28 -0300

    Programacao 2

---
+++ CadastroUsuario/src/br/com/CadastroUsuario/Usuario.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.CadastroUsuario;
+
+/**
+ *
+ * @author Samuel
+ */
+public class Usuario {
+    
+    private String nome;
+    private String email;
+    private String cpf;
+    private int idade;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" + "nome=" + nome + ", email=" + email + ", cpf=" + cpf + ", idade=" + idade + '}';
+    }
+
+  
+    
+    
+    
+}
