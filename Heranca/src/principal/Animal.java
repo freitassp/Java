28ba4d2df54b35836a230a48a9090d69a5d6c8f8
freitassp <priettosamuel@gmail.com>
2020-10-19 20:09:43 -0300

    Praticando Herança

---
+++ Heranca/src/principal/Animal.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package principal;
+
+/**
+ *
+ * @author Samuel
+ */
+public abstract  class Animal {
+    private String tipo;
+    private String nome;
+    
+    
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public void som(){
+        System.out.println("Principal");
+    }
+}
