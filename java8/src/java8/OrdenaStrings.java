709702548a77e6438ec539029bb278497e0090e2
freitassp <priettosamuel@gmail.com>
2020-11-17 23:12:33 -0300

    cnsumer lambda

--- java8/src/java8/OrdenaStrings.java
+++ java8/src/java8/OrdenaStrings.java
@@ -10,57 +10,101 @@ public class OrdenaStrings {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+
 		List<String> palavras = new ArrayList<String>();
 		palavras.add("alura online");
 		palavras.add("editora casa do codigo");
 		palavras.add("caelum");
+
+//		Comparator<String> comparator = new ComparadorPorTamanho();
+//		// Collections.sort(palavras,comparator);
+//		palavras.sort(comparator);
+//		System.out.println(palavras);
+//		
+
+//		palavras.sort(new Comparator<String>() {
+//
+//			@Override
+//			public int compare(String s1, String s2) {
+//				if (s1.length() < s2.length())
+//					return -1;
+//
+//				if (s2.length() > s1.length())
+//					return 1;
+//
+//				return 0;
+//			}
+//		});
+//		
+
+//		palavras.sort((s1, s2) -> {
+//			if (s1.length() < s2.length())
+//				return -1;
+//
+//			if (s2.length() > s1.length())
+//				return 1;
+//
+//			return 0;
+//
+//		});
 		
-		Comparator<String> comparator = new ComparadorPorTamanho();
-		//Collections.sort(palavras,comparator);
-		palavras.sort(comparator);
-		System.out.println(palavras);
-		
+		palavras.sort((s1,s2)-> Integer.compare(s1.length(), s2.length()));
+
 //		for (String p : palavras) {
 //			System.out.println(p);
 //		}
-		
-		
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
+
+//		Consumer<String> consumidor = new Consumer<String>() {
+//
+//			@Override
+//			public void accept(String s) {
+//				System.out.println(s);
+//				
+//			}
+//			
+//		};
+
+		palavras.forEach(s -> System.out.println(s)
+
+		);
+
+//				
+//		palavras.forEach(new Consumer<String>() {
+//
+//			@Override
+//			public void accept(String s) {
+//				System.out.println(s);
+//				
+//			}
+//		});
 
 	}
 
 }
 
-class ImprimeNaLinha implements Consumer<String> {
+//essa classe foi esumida acima criando assim uma classe anonima
+//class ImprimeNaLinha implements Consumer<String> {
+//
+//	@Override
+//	public void accept(String s) {
+//		System.out.println(s);
+//		
+//	}
 
-	@Override
-	public void accept(String s) {
-		System.out.println(s);
-		
-	}
-	
-	
-	
-}
+//}
 
 class ComparadorPorTamanho implements Comparator<String> {
 
 	@Override
 	public int compare(String s1, String s2) {
-		
-		if(s1.length() < s2.length())
+
+		if (s1.length() < s2.length())
 			return -1;
-		
-		if(s2.length() > s1.length())
+
+		if (s2.length() > s1.length())
 			return 1;
-		
+
 		return 0;
 	}
-	
 
-	
-	
-	
 }
